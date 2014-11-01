e91f5301aa179a6e934545a70c1af41dbcd9ce83
Stephan Detje <stephan.detje@student.hpi.de>
2014-11-01 13:31:11 +0000

    add a full title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+    # Return the full application title on a per-page basis
+    def fullTitle(pageTitle = '')
+        baseTitle = "Ruby on Rails Tutorial Sample App"
+        if pageTitle.empty?
+            baseTitle
+        else
+            "#{pageTitle} | #{baseTitle}"
+        end
+    end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,21 +1,26 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  
+  def setup
+    @baseTitle = "Ruby on Rails Tutorial Sample App"
+  end
+  
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "#{@baseTitle}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Help | #{@baseTitle}"
   end
   
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "About | #{@baseTitle}"
   end
 end
