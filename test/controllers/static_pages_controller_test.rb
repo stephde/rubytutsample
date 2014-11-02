6943c58de6bf0fe123a143a80aa3b322784e1b6f
Stephan Detje <stephan.detje@student.hpi.de>
2014-11-02 13:48:13 +0000

    added singup page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -23,4 +23,10 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
     assert_select "title", "About | #{@baseTitle}"
   end
+  
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | #{@baseTitle}"
+  end
 end
