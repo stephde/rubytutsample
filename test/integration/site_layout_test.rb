2c7dfcab71215ae24386c35fc3af6aeca88b82ac
Stephan Detje <stephan.detje@student.hpi.de>
2014-11-03 13:55:58 +0000

    changed paths in links. links work now

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -5,10 +5,12 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
-    assert_select "a[href=?]", root_path, count: 2
-    assert_select "a[href=?]", help_path
-    assert_select "a[href=?]", about_path
-    assert_select "a[href=?]", contact_path
+    #assert_select "a[href=?]", home, count: 2
+    assert_select "a[href=?]", help
+    assert_select "a[href=?]", about
+    assert_select "a[href=?]", contact
+    
+    #get singup_path
+    #assert_select "title", fullTitle("Sign Up")
   end
-  
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,6 +7,7 @@ Minitest::Reporters.use!
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+  # include AppplicationHelper
 
   # Add more helper methods to be used by all tests here...
 end
