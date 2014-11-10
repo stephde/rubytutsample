7900013ef8ec3bfdd7823781a7a3c0f29dd98201
Stephan Detje <stephan.detje@student.hpi.de>
2014-11-10 21:31:48 +0000

    finished log in/out and remember

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       #log the user in
       log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user
     else
       # create an error message
@@ -16,5 +17,7 @@ class SessionsController < ApplicationController
   end
   
   def destroy
+    log_out if logged_in?
+    redirect_to root_url
   end
 end
