f4bdec4ae0f5873fd51fa237d9e21051892445d2
Stephan Detje <stephan.detje@student.hpi.de>
2014-11-10 23:16:40 +0000

    started implementing authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
   attr_accessor :name, :email
   
   def show
@@ -22,8 +24,37 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+  end
+  
+  def update
+    if(@user.update_attributes(user_params))
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
   private 
-    def user_params
+    def user_param
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
+    
+  # Before filters
+
+  # Confirms a logged-in user.
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+
+  # Confirms the correct user.
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless @user == current_user
+  end
+  
 end
